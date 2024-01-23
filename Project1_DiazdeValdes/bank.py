9187c71590047b4b1f04e9390d1cb3ac9f3a3b5a
Sadieddv <157528697+Sadieddv@users.noreply.github.com>
2024-01-23 12:10:18 -0700

    Add files via upload

---
+++ Project1_DiazdeValdes/bank.py
@@ -0,0 +1,19 @@
+class CheckingAccount:
+    # class body
+    pass
+
+class SavingAccount:
+    # class body
+    pass
+
+
+class Employee:
+   pass
+
+class Employee:
+   def __init__(self,name,age):
+    self.name = name
+    self.age = age
+    #create employee object
+    employee1 = Employee("Sadie", 19)
+    print(employee1.name)
